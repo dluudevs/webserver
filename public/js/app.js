ac74557abbc650f24b2f2165a68896eb2c1fd901
Derek <derekluu90@gmail.com>
2020-05-20 18:06:40 -0400

    print results to DOM, add styling to form

--- public/js/app.js
+++ public/js/app.js
@@ -2,18 +2,24 @@ console.log('Client side JavaScript is loaded')
 
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
 
 weatherForm.addEventListener('submit', (e) => {
   e.preventDefault();
   // fetch is a browser api. JavaScript runs in browser, but node doesnt (hence we need request)
+  messageOne.textContent = 'Loading...'
+  messageTwo.textContent = ''
+  
   fetch(`/weather?address=${search.value}`).then(res => {
     res.json().then(data => {
       if (data.error){
-        return console.log(data.error)
+        return messageOne.textContent = data.error
       }
 
-      console.log(data.location)
-      console.log(data.forecast)
+      messageOne.textContent = data.location
+      messageTwo.textContent = data.forecast
     })
   })
 })
