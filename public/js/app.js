b2d9e4da60795436ae5f3e79a694dd16497199a8
Derek <derekluu90@gmail.com>
2020-05-20 17:05:25 -0400

    added api call to weather endpoint in  client side script

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,13 @@
 console.log('Client side JavaScript is loaded')
+
+// fetch is a browser api. JavaScript runs in browser, but node doesnt (hence we need request)
+  fetch(' /weather?address=Markham').then(res => {
+    res.json().then(data => {
+      if (data.error){
+        return console.log(data.error)
+      }
+
+      console.log(data.location)
+      console.log(data.forecast)
+    })
+  })
